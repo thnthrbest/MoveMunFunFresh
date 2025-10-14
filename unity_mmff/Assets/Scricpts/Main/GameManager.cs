bacc84d0aff580776710afb4c3c0a30625d1ebb8
Jap-SAt <japsuperdog527@gmail.com>
2025-10-14 21:21:47 +0700

    add_GameDescription

--- unity_mmff/Assets/Scricpts/Main/GameManager.cs
+++ unity_mmff/Assets/Scricpts/Main/GameManager.cs
@@ -10,6 +10,7 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    private int game_id;
     public TextMeshProUGUI Gamename;
     [System.Serializable]
     public class GameNameList
@@ -18,6 +19,7 @@ public class GameManager : MonoBehaviour
     }
     [Header("GameName List")]
     public List<GameNameList> Names = new List<GameNameList>();
+
     public Image GameImage;
 
     [System.Serializable]
@@ -40,28 +42,47 @@ public class GameManager : MonoBehaviour
 
     public void NextSelectGame()
     {
-        if (GameImages.Count == 0) return;
+        if (GameImages.Count == 0 || Names.Count == 0) return;
         Currentindex++;
         if(Currentindex >= GameImages.Count)
         {
             Currentindex = 0;
         }
         UpdateGameImage();
+        UpdateText();
+        GameID(Currentindex);
     }
 
     public void PreviousSelectGame()
     {
-        if (GameImages.Count == 0) return;
+        if (GameImages.Count == 0 || Names.Count == 0) return;
         Currentindex--;
         if(Currentindex <= 0)
         {
             Currentindex = GameImages.Count - 1 ;
         }
         UpdateGameImage();
+        UpdateText();
+        GameID(Currentindex);
     }
-    
+
+    public void GameID()
+    {
+        game_id = Currentindex + 1;
+    }
+
+    public int GetGameID()
+    {
+        return game_id;
+    }
     private void UpdateGameImage()
     {
         GameImage.sprite = GameImages[Currentindex].GameListSprite;
     }
+
+    private void UpdateText()
+    {
+        Gamename.text = Names[Currentindex].GameName;
+    }
+
 }

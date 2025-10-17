0ce79aec3e435f2280c1c5794246b77decdb0673
thnthrbest <106542860+thnthrbest@users.noreply.github.com>
2025-10-17 21:57:15 +0700

    finish hm

--- unity_mmff/Assets/Scricpts/hand_made/startServer.cs
+++ unity_mmff/Assets/Scricpts/hand_made/startServer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 using System.Diagnostics;
 using TMPro;
@@ -16,6 +17,7 @@ public class startServer : MonoBehaviour
     [SerializeField] private manange manange;
     [SerializeField] private UDPReceive UDPReceive;
     [SerializeField] private TextMeshProUGUI score;
+    public RawImage loading;
     int temp=0;
 
     IEnumerator StartServer()
@@ -29,17 +31,19 @@ public class startServer : MonoBehaviour
             CountdownTimer.enabled = true;
             yield return new WaitForSeconds(3);
             StartCoroutine(re());
+            yield return new WaitForSeconds(2);
+            loading.gameObject.SetActive(false);
             //UnityEngine.Debug.Log("send");
         }
     }
     IEnumerator re()
     {
         while(true){
-
+            int value = Random.Range(4,9); 
             sendstate.send();
             yield return new WaitForSeconds(1f);
             StartCoroutine(manange.change_img(UDPReceive.a[0]));
-            yield return new WaitForSeconds(10f);
+            yield return new WaitForSeconds(value);
             temp++;
             score.text = temp.ToString();
         }

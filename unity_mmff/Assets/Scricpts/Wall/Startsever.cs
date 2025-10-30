f0096e7836bab6627fc0dbcf70a33bea60c7bc93
Banana2077 <29656@sa.ac.th>
2025-10-31 00:37:10 +0700

    Add Timer

--- unity_mmff/Assets/Scricpts/Wall/Startsever.cs
+++ unity_mmff/Assets/Scricpts/Wall/Startsever.cs
@@ -7,18 +7,23 @@ public class StartServer : MonoBehaviour
 {
     // Start is called before the first frame update
     public DecSeen dec;
+    public GameObject ls;
+    public TimeCounter tc;
     public void Start()
     {
 
         StartCoroutine(StartUP());
-        
 
     }
 
     IEnumerator StartUP(){ //ใช้สำหรับหน่วงเวลา
-        Process.Start("D:/MoveMunFunFresh/MyMediapipe/run.bat");
-        yield return new WaitForSeconds(5);
+        Process.Start("C:/Users/comsc/Documents/GitHub/MoveMunFunFresh/MyMediapipe/run.bat");
+        yield return new WaitForSeconds(8);
         dec.enabled = true;
+        yield return new WaitForSeconds(2);
+        ls.SetActive(false);
+        tc.enabled = true;
+
         
     }
     
